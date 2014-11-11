9641b55e25f15bda839b63a4b8f897ff99cdff88
abocz <aboczjr@gmail.com>
2014-11-11 13:08:23 -0500

    set port for travis and setup xvfb

--- src/test/java/com/redhat/darcy/webdriver/FirefoxBrowserFactoryTest.java
+++ src/test/java/com/redhat/darcy/webdriver/FirefoxBrowserFactoryTest.java
@@ -6,6 +6,7 @@ import com.redhat.darcy.webdriver.testing.rules.TraceTestName;
 import org.junit.Rule;
 import org.junit.Test;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertThat;
@@ -18,6 +19,21 @@ public class FirefoxBrowserFactoryTest {
     public void shouldBeInstanceOfUntargetedFirefoxDriver() {
         WebDriverBrowserFactory browserFactory = new FirefoxBrowserFactory();
 
+        WebDriverBrowser browser = (WebDriverBrowser) browserFactory.newBrowser();
+        ForwardingTargetedWebDriver webDriver = (ForwardingTargetedWebDriver) browser.getWrappedDriver();
+        CachingTargetLocator targetLocator = (CachingTargetLocator) webDriver.getTargetLocator();
+
+        assertThat(targetLocator.getUntargetedDriver(), instanceOf(FirefoxDriver.class));
+        browser.close();
+    }
+
+    @Test
+    public void shouldBeFireFoxin() {
+
+        FirefoxBrowserFactory browserFactory = new FirefoxBrowserFactory();
+        DesiredCapabilities cap=new DesiredCapabilities();
+        cap.setCapability("webdriver_firefox_port",7046);
+        browserFactory = browserFactory.desiring(cap);
         WebDriverBrowser browser = (WebDriverBrowser) browserFactory.newBrowser();
         ForwardingTargetedWebDriver webDriver = (ForwardingTargetedWebDriver) browser.getWrappedDriver();
         CachingTargetLocator targetLocator = (CachingTargetLocator) webDriver.getTargetLocator();
